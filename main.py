56f0024a0e0686d1e5d639e0fdcc1122ceea03ee
Bill Phan <billphan25@gmail.com>
2017-06-24 14:08:54 -0700

    Adjusting paragraph font sizing.

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ form = """
                 color: #000;
                 display: block;
                 margin: 0 auto;
-                font-size: 16px;
+                font-size: 14px;
                 text-align: left;
                 font-weight: 200;
                 padding-bottom: 20px;
