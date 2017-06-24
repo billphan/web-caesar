9bb36da0654b2c4906b2e16a62178d53e8fd045f
Bill Phan <billphan25@gmail.com>
2017-06-24 14:10:12 -0700

    Adjusted font sizing and added Helvetica as fallback font

--- main.py
+++ main.py
@@ -68,11 +68,11 @@ form = """
                 color: #000;
                 display: block;
                 margin: 0 auto;
-                font-size: 14px;
+                font-size: 16px;
                 text-align: left;
                 font-weight: 200;
                 padding-bottom: 20px;
-                font-family: 'Roboto', sans-serif;
+                font-family: 'Roboto', 'Helvetica', sans-serif;
             }}
 
             hr {{
